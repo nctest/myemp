9a65ca8e274428db87cb7e107fecad8fb8ef3eab
nctest <nctest@nt.com>
2012-09-04 17:23:56 +0800

    add method_config.xml and MethodModelService.java files

---
+++ src/client/nc/ui/myemp/method/model/MethodModelService.java
@@ -0,0 +1,33 @@
+package nc.ui.myemp.method.model;
+
+import nc.ui.uif2.model.IAppModelService;
+import nc.vo.uif2.LoginContext;
+
+public class MethodModelService implements IAppModelService {
+
+	@Override
+	public Object insert(Object object) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object update(Object object) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object object) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object[] queryByDataVisibilitySetting(LoginContext context)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
